a2401e8c5cf076d864a7b2bbdea3d97c851941a0
hzsong <tianfangsong123@163.com>
2017-06-08 09:49:22 +0800

    增加cnn top3验证代码，测试一下

--- cnn/train_cnn.py
+++ cnn/train_cnn.py
@@ -19,7 +19,7 @@ tf.flags.DEFINE_string("label_file", "../../data/data_by_ocean/eclipse/fixer.csv
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "10,100,200", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.2, "L2 regularization lambda (default: 0.0)")
