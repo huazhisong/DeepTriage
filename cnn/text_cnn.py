6ee023eff7a871294c86351a70a5e4fd87b9ca3c
hzsong <tianfangsong123@163.com>
2017-06-11 16:04:05 +0800

    增加tensorflow自带precision@3函数

--- cnn/text_cnn.py
+++ cnn/text_cnn.py
@@ -86,5 +86,5 @@ class TextCNN(object):
         # Evaluation
         with tf.name_scope("precision"):
             precision_pre = tf.contrib.metrics.streaming_sparse_precision_at_k(tf.cast(self.scores, tf.float32),
-                                                                                   tf.argmax(self.input_y, 1), 3)
+                                                                               tf.argmax(self.input_y, 1), 3)
             self.precision = tf.reduce_mean(tf.cast(precision_pre, "float"), name="precision")
