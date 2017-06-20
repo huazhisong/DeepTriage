8486ac32cca3e53ae0cfea63d2236e5eacea2d97
hzsong <tianfangsong123@163.com>
2017-06-20 14:16:02 +0800

    使用static方式进行训练

--- cnn/train_cnn.py
+++ cnn/train_cnn.py
@@ -38,7 +38,7 @@ tf.flags.DEFINE_integer("top_k", 3, "evaluation top k")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-tf.flags.DEFINE_string("embedding_type", "train", "static,train,static_train (default: 'static')")
+tf.flags.DEFINE_string("embedding_type", "static", "static,train,static_train (default: 'static')")
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
